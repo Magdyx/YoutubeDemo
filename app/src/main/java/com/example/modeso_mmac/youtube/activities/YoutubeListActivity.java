867fc73a0854ddc4579a8eb32da21e1919b8a1d1
Belal Mohamed <belalmohamed92@gmail.com>
2016-08-22 22:44:48 +0200

    Update gradle version.

--- app/src/main/java/com/example/modeso_mmac/youtube/activities/YoutubeListActivity.java
+++ app/src/main/java/com/example/modeso_mmac/youtube/activities/YoutubeListActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.example.modeso_mmac.youtube.fragments.ListFragment;
 import com.example.modeso_mmac.youtube.R;
